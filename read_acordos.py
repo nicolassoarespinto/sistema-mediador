423b888ccbd728c2fcc9f6f7aabf658354e5fde1
Nicolas Pinto <>
2022-01-21 13:19:16 -0600

    - Add extraction of cnpjs and descriptions - Fix bug on json export

--- read_acordos.py
+++ read_acordos.py
@@ -3,6 +3,7 @@ import re
 import os
 # import beutiful soup package
 import bs4 as bs
+import json
 
 
 # Read metadatafile of acordos
@@ -22,11 +23,21 @@ def checkIfDownloaded(n_solic):
     else:
         return False
 
-def getClausulas(soup):
+def getClausulasDescr(soup):
+    # Create a vector with the text of each element of soup.find_all('label', {'class' : 'descricaoClausula'})	
+    result = [piece.get_text() for piece in soup.find_all('label', {'class' : 'descricaoClausula'} )]
+    # Clean extra spaces and /n from the text
+    result = [re.sub('\n', '', re.sub('\s+', ' ', piece.strip())) for piece in result]
+    # Remove empty strings from the result vector
+    #result = [piece for piece in result if piece != '']
+
+    return result
+
+def getClausulasTitles(soup):
     # Create a vector with the text of each element of soup.find_all('label', {'class' : 'tituloClausula'})	
     result = [piece.get_text() for piece in soup.find_all('label', {'class' : 'tituloClausula'} )]
-    # Clean extra spaces and /n from the text
-    result = [re.sub('\n', '', re.sub('\s+', ' ', piece)) for piece in result]
+    # Clean extra spaces and /n from the text and trim spaces 
+    result = [re.sub('\n', '', re.sub('\s+', ' ', piece.strip())) for piece in result]
     # Remove empty strings from the result vector
     result = [piece for piece in result if piece != '']
 
@@ -34,10 +45,18 @@ def getClausulas(soup):
 
 
 
+
+
 def getCNPJS(soup):
 
     # Find the 'p'tags with attribute 'align' = 'justify' and get the text inside
-    html_text = soup.find_all('p', {'align' : 'justify'})[0].get_text()
+    tags = soup.find_all('p', {'align' : 'justify'})
+    
+    # If no tag is found, return an empy list
+    if len(tags) == 0:
+        return []
+
+    html_text = tags[0].get_text()
  
 
     # Use regular expressions to extract all sequences of numbers similar to 11.416.654/0001-17 and 00.342.957/0001-16
@@ -61,13 +80,14 @@ def readAcordoInfo(n_solic):
         soup = bs.BeautifulSoup(html, 'html.parser')
         
         # Get clausulas of soup
-        clausulas = getClausulas(soup)
+        clausulasTitles = getClausulasTitles(soup)
+        clausulasDescr = getClausulasDescr(soup)
 
         # Get CNPJs of soup
         cnpjs = getCNPJS(soup)
 
         # return a dictionary with key n_solic = n_solic, and clausulas = clausulas and cnpjs = cnpjs
-        return {'n_solic' : n_solic, 'clausulas' : clausulas, 'cnpjs' : cnpjs}    
+        return {'n_solic' : n_solic, 'clausulasDescr' : clausulasDescr, 'clausulasTitles' : clausulasTitles, 'cnpjs' : cnpjs}    
 
         # return pd.DataFrame({'n_solic' : n_solic, 'clausulas' : clausulas, 'cnpjs' : cnpjs})
         
@@ -88,5 +108,5 @@ for n_solic in acordos['n_solic']:
 
 # Save the dictionary
 with open('data/acordos/acordos_info.json', 'w') as f:
-    f.write(str(acordos_info))
+    json.dump(acordos_info, f, indent= 4)
     
